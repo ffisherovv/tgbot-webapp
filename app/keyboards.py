12f0038212dc41e372717d0d17ea67e9fde1306b
Асылжан <asylzhan.sarsenbay_3@mail.ru>
2024-09-02 22:19:13 +0500

    changed the text of pizza and changed the link of webapp

--- app/keyboards.py
+++ app/keyboards.py
@@ -4,5 +4,5 @@ from aiogram.types.web_app_info import WebAppInfo
 
 
 markup = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Перейти на веб-сайт.',web_app=WebAppInfo(url='https://ffisherovv.github.io/myproject/'))]
+    [KeyboardButton(text='Перейти на веб-сайт.',web_app=WebAppInfo(url='https://ffisherovv.github.io/tgbot-webapp/'))]
 ])
