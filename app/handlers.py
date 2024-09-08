e60cfbea221a531cdcec9eadac388745efde9579
Асылжан <asylzhan.sarsenbay_3@mail.ru>
2024-09-08 22:54:19 +0500

    JSON 2

--- app/handlers.py
+++ app/handlers.py
@@ -1,15 +1,14 @@
-from aiogram import F,Router
-from aiogram.filters import CommandStart,Command,ContentType
-from aiogram.types import Message,CallbackQuery
+from aiogram import F, Router
+from aiogram.filters import CommandStart, Command  # Исправлен импорт ContentTypes
+from aiogram.types import Message,ContentType
 import app.keyboards as kb
 
 router = Router()
 
 @router.message(CommandStart())
-async def start(message:Message):
-    await message.answer("Hello! Visit out website.",reply_markup=kb.markup)   
+async def start(message: Message):
+    await message.answer("Hello! Visit our website.", reply_markup=kb.markup)
 
-@router.message(F.content_type == ContentType.WEB_APP_DATA) 
-async def webapp(message:Message):
+@router.message(F.content_type == ContentType.WEB_APP_DATA)  # Используем ContentTypes
+async def webapp(message: Message):
     await message.answer(message.web_app_data.data)
-    
